263aed6ce1113412b2175d611c43e26472f772e7
Junn28 <juniarda28@gmail.com>
2023-05-29 23:06:53 +0700

    first init

---
+++ authentication-migrate/app/migrations/20230528015558-create-status.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('statuses', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      body: {
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'RESTRICT',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('statuses');
+  },
+};
